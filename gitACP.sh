abb1ebaf9c1225267ab9ea52ee97543b1ee42006
Whatmelon <ccacrop@gmail.com>
2024-12-06 06:54:37 +0800

    Upper case of the shell bash

--- gitACP.sh
+++ gitACP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置默认的 commit message
-DEFAULT_MSG="default commit message"
+DEFAULT_MSG="Default commit message"
 
 # 使用传入的参数作为 commit message，否则使用默认值
 COMMIT_MSG="${1:-$DEFAULT_MSG}"
